3a5e3313cbb35f33f812438336566da60da25e43
Isaac Ratner <isaacratner@gmail.com>
2017-03-05 11:09:16 -0500

    refactored editButton to be a stateless functional component

--- app/editButton.js
+++ app/editButton.js
@@ -1,25 +1,14 @@
 const React = require('react');
 
-class EditButton extends React.Component {
-    constructor(props) {
-        super(props);
-        this.toggleEditing = this.toggleEditing.bind(this);
-    }
-
-    toggleEditing() {
-        this.props.handleClick();
-    }
-
-	render() {
-  	    return (
-            <input 
-                type="button"
-                value={this.props.updating ? "click when done editing" : "click to start editing"}
-                onClick={this.toggleEditing}
-                className="btn btn-default btn-lg"
-            />
-        )
-    }
+function EditButton(props) {
+    return (
+        <input 
+            type="button"
+            value={props.updating ? "click when done editing" : "click to start editing"}
+            onClick={props.handleClick}
+            className="btn btn-default btn-lg"
+        />
+    )
 }
 
 module.exports = EditButton;
